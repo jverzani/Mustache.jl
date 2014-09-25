c2e461099930c439ba4bfb095b7c285eb6c89ca2
jverzani <jverzani@gmail.com>
2014-09-25 13:52:40 -0400

    fix tests to pass checking

---
+++ test/data-frame-test.jl
@@ -0,0 +1,35 @@
+## Data frame test
+## not run by default. Too time consuming and relies on external pacakgs
+
+glm_tpl = mt"""
+\begin{table}
+\begin{tabular}{l @{\quad} rrrr}
+{{#colnms}}
+&\verb+{{{colnm}}}+
+{{/colnms}}\\
+{{#mat}}
+{{variable}} & {{col1}} & {{col2}} & {{col3}} & {{col4}}\\
+{{/mat}}
+\end{tabular}
+\end{table}
+"""
+
+function glm_table(mod)
+    tbl = coeftable(mod)
+
+    colnms = DataFrame(colnm=tbl.colnms)
+    mat = DataFrame(variable=tbl.rownms)
+    for j in 1:size(tbl.mat)[2]
+        nm = "col$j"
+        mat[symbol(nm)] = map(x -> @sprintf("%.2f", x), tbl.mat[:,j])
+    end
+    
+    Mustache.render(glm_tpl,{"colnms"=>colnms, "mat"=>mat})
+end
+
+
+
+using GLM, RDatasets, DataFrames
+LifeCycleSavings = dataset("datasets", "LifeCycleSavings")
+fm2 = fit(LinearModel, SR ~ Pop15 + Pop75 + DPI + DDPI, LifeCycleSavings)
+glm_table(fm2)
