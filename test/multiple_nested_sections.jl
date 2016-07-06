090cd6e412aa4e82a9eff8e74fd51258ba815f70
jverzani <jverzani@gmail.com>
2016-07-05 20:37:03 -0400

    test for #31

--- test/multiple_nested_sections.jl
+++ test/multiple_nested_sections.jl
@@ -27,3 +27,29 @@ test_func = () -> render(failing_template,
 	                              "times2" => test_data("x", 4:8)))
                                       res = test_func()
 @assert res == "[1,[4,5,6,7,8,],2,[4,5,6,7,8,],3,[4,5,6,7,8,],]"
+
+
+## issue #31 -- nested sections
+type Location
+  lat::Float64
+  lon::Float64
+end
+type Thing
+  location::Location
+  name::AbstractString
+end
+x = [Thing(Location(1.,2.), "name"), Thing(Location(3.,4.), "nombre")]
+
+
+tpl = """
+{{#:x}}
+{{#.}}
+{{#location}}
+{{lat}}--{{lon}}
+{{/location}}
+{{name}}
+{{/.}}
+{{/:x}}
+"""
+
+Mustache.render(tpl, x=x)  == "\n\n\n1.0--2.0\n\nname\n\n\n\n\n3.0--4.0\n\nnombre\n\n\n"
