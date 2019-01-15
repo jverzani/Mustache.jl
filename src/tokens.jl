8c0b4f55a1e2922a7e803fa48b80ff74efde7b43
jverzani <jverzani@gmail.com>
2019-01-15 12:28:34 -0500

    edits

--- src/tokens.jl
+++ src/tokens.jl
@@ -434,8 +434,9 @@ end
 function renderTokensByValue(value, io, token, writer, context, template, args...)
     if Tables.istable(value)
         for row in Tables.rows(value)
-            #renderTokens(io, token.collector, writer, ctx_push(context, row), template, args...)
-            renderTokens(io, token.collector, writer, ctx_push(context, getfield(row,1)), template, args...)
+            @show "try this", row, token
+#            _renderTokensByValue(getfield(row,1), io, token, writer, context, template, args...)
+            renderTokens(io, token.collector, writer, ctx_push(context, row), template, args...)
         end
     elseif is_dataframe(value) # XXX remove once istable(x::DataFrame) == true works
         for i in 1:size(value)[1]
