cff1f065b2ac69d1b3e763cc2b8e6e3aa9b76d2f
john verzani <jverzani@gmail.com>
2022-07-18 11:39:26 -0400

    link docs to dev, not stable (#148)

--- src/Mustache.jl
+++ src/Mustache.jl
@@ -1,7 +1,7 @@
 """
 Mustache
 
-[Mustache](https://github.com/jverzani/Mustache.jl) is a templating package for `Julia` based on [Mustache.js](http://mustache.github.io/). [ [Docs](https://jverzani.github.io/Mustache.jl/stable/) ]
+[Mustache](https://github.com/jverzani/Mustache.jl) is a templating package for `Julia` based on [Mustache.js](http://mustache.github.io/). [ [Docs](https://jverzani.github.io/Mustache.jl/dev/) ]
 """
 module Mustache
 
