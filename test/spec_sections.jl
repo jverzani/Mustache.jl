a51d484c1774fd0d24851b3a1a729bc22da33618
tz-lom <vzorvat@gmail.com>
2023-06-13 18:26:10 +0200

    Support variables from root context (#153)
    * Support variables from root context
    
    * Update spec_sections.jl
    
    ---------
    
    Co-authored-by: tz-lom <nuzhdin.urii@gmail.com>

--- test/spec_sections.jl
+++ test/spec_sections.jl
@@ -203,6 +203,12 @@ tpl = """#{{#boolean}}
 tpl = """|{{# boolean }}={{/ boolean }}|"""
 
 	@test Mustache.render(tpl, Dict{Any,Any}("boolean"=>true)) == """|=|"""
+
+
+	## Nested object fields should be accessible via global lookup
+tpl = """|{{# inner }}{{ field.subfield }}{{/ inner }} {{# inner }}{{ ~field.subfield }}{{/ inner }}|"""
+
+	@test Mustache.render(tpl, Dict{Any,Any}("inner"=>Dict("field"=>Dict("subfield"=>1)),"field"=>Dict("subfield"=>2))) == """|1 2|"""
 end
 
 
