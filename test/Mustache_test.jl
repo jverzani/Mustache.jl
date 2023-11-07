aece53e8e72272f00aeaa41405102da074436538
john verzani <jverzani@gmail.com>
2023-11-07 13:38:30 -0500

    fix compat bounds (#161)
    * fix compat bounds
    
    * fix test
    
    * keyword defaults no in 1.0

--- test/Mustache_test.jl
+++ test/Mustache_test.jl
@@ -322,8 +322,8 @@ tpl2 = mt"""
 """
     vec = [Dict("name" => x) for x in ("a", "b")]
     _uppercase(str, render) = uppercase(render(str))
-    a = Mustache.render(Mustache.parse(tpla, ("<<", ">>")); vec, uppercase=_uppercase)
-    b = Mustache.render(Mustache.parse(tplb); vec, uppercase=_uppercase)
+    a = Mustache.render(Mustache.parse(tpla, ("<<", ">>")); vec=vec, uppercase=_uppercase)
+    b = Mustache.render(Mustache.parse(tplb); vec=vec, uppercase=_uppercase)
     @test a == b
 
 end
